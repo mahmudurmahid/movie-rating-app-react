a4f41765fedd0c72f02f9b64692fb0fcfeaae03a
mahmudurmahid <m.mahid@icloud.com>
2025-07-14 14:44:54 +0100

    changed what is shown in index.js (ie view) from star rating to actual app

--- movie-rating-app/src/index.js
+++ movie-rating-app/src/index.js
@@ -1,20 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-// import "./index.css";
-// import App from "./App";
+import "./index.css";
+import App from "./App";
 import StarRating from "./StarRating";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    {/* <App /> */}
-    <StarRating />
+    <App />
+    {/* <StarRating />
     <StarRating
       maxRating={40}
       size={24}
       color="red"
       className="test"
       defaultRating={3}
-    />
+    /> */}
   </React.StrictMode>
 );
