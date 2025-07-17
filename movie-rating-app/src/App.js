04fef0d96eb4f16ee777a8e772ce9bdba0f82675
mahmudurmahid <m.mahid@icloud.com>
2025-07-17 16:39:16 +0100

    movies with existing ratings have message to show this

--- movie-rating-app/src/App.js
+++ movie-rating-app/src/App.js
@@ -135,6 +135,7 @@ export default function App() {
                 selectedId={selectedId}
                 onCloseMovie={handleCloseMovie}
                 onAddWatched={handleAddWatched}
+                watched={watched}
               />
             ) : (
               <>
@@ -263,10 +264,12 @@ function Movie({ movie, onSelectMovie }) {
   );
 }*/
 
-function MovieDetails({ selectedId, onCloseMovie, onAddWatched }) {
+function MovieDetails({ selectedId, onCloseMovie, onAddWatched, watched }) {
   const [movie, setMovie] = useState({});
   const [isLoading, setIsLoading] = useState(false);
   const [userRating, setUserRating] = useState("");
+
+  const isWatched = watched.map((movie) => movie.imdbID).includes(selectedId);
 
   const {
     Title: title,
@@ -337,15 +340,21 @@ function MovieDetails({ selectedId, onCloseMovie, onAddWatched }) {
 
           <section>
             <div className="rating">
-              <StarRating
-                maxRating={10}
-                size={24}
-                onSetRating={setUserRating}
-              />
-              {userRating > 0 && (
-                <button className="btn-add" onClick={handleAdd}>
-                  Add to List
-                </button>
+              {!isWatched ? (
+                <>
+                  <StarRating
+                    maxRating={10}
+                    size={24}
+                    onSetRating={setUserRating}
+                  />
+                  {userRating > 0 && (
+                    <button className="btn-add" onClick={handleAdd}>
+                      Add to List
+                    </button>
+                  )}{" "}
+                </>
+              ) : (
+                <p>Looks like you’ve already rated this one!</p>
               )}
             </div>
             {/* <div className="rating">
